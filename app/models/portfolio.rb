5dd4c96eb2f8f74060dfd8b3343a42e5a2bcbeab
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-14 15:56:19 +0100

    Add image default values to portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,16 @@ class Portfolio < ApplicationRecord
 
   #Define a scope instead of a method to make a query
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
+
+  #self.main_image ||= "http://placehold.it/600x400" replaces the following code:
+  #if self.main_image == nil
+  #. self.main_image = http://placehold.it/600x400
+  #end
 end
