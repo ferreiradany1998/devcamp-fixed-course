cce3cf1531eaaf36b9928787d8f71ccf17175e03
Development Dany <development_dany@195-190-169-108.vpn-mur-pal.ticino.com>
2020-03-16 09:21:00 +0100

    Completed Authentication Implementation with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #They must specify a name before registering, otherwise they will run into an error on first_name, last_name methods
+  validates_presence_of :name
+
+  def first_name
+    #Grab the first piece of information before the space
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
