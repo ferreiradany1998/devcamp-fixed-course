3484319e93daa54d59b32406955268ec3a182b3c
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-16 16:01:15 +0100

    Integrated helper methods from ActionView

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,5 +5,5 @@ class Blog < ApplicationRecord
   #Checks that body and title exist in the create command
   validates_presence_of :title, :body
 
-  belongs_to :topic
+  belongs_to :topic, optional: true
 end
