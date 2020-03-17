83cb717f944b35d2c863e07e6a8f777e2e0fc115
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-17 14:51:10 +0100

    Updated auth to include draft and published status toggle controls

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   #What actions we want everyone can access
   #What can't a regular user do
   #site_admin can do everything
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
