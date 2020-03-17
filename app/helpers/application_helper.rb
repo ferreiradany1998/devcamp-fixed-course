14d30a3b4ecb70fa8c911a336981cc5b4641b79b
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-17 17:13:30 +0100

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,58 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_me_path,
+        title: "About Me"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      }
+    ]
+  end
+
+  #Iterate through the nav_items array and for each print html code
+  def nav_helper style, tag_type
+    nav_links = ""
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  #This allows us to have a big string interpelation.
+  #It must start at the LEFT SIDE of the IDE
+  #This is a way to create navbar items by passing as parameter style and tagtype
+#  def nav_helper style, tag_type
+#nav_links = <<NAV
+#<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+#<#{tag_type}><a href="#{about_me_path}" class="#{style} #{active? about_me_path}">About Me</a></#{tag_type}>
+#<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+#<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+#<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+#NAV
+ #   nav_links.html_safe
+  #end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
