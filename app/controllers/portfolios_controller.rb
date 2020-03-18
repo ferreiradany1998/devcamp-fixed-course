2a1a3b117b491096f16a51e01881186458210526
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-18 13:25:39 +0100

    Integrated cacoon for portfolio items nested form components

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -29,7 +29,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
   def edit
@@ -83,7 +82,7 @@ class PortfoliosController < ApplicationController
                                     :body,
                                     :main_image,
                                     :thumb_image, 
-                                    technologies_attributes: [:name])
+                                    technologies_attributes: [:id, :name, :_destroy]) #_destroy allows us to destroy the item
   end
 
   def set_portfolio_item
