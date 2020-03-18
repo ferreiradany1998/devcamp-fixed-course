2d032dcd87488c1933db0625fcac6396b553f15b
Development Dany <development_dany@Danys-MBP.fritz.box>
2020-03-18 14:01:51 +0100

    Refactored alers into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -81,4 +81,24 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+
+#Is there a flash alert? If not is there a flash error? If not is there a flash notice? If not returns nil
+#if there is one of them, it returns true and the variable gets saved into alert
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Dany Coelho Ferreira Portfolio", sticky: false, time: 10000)
+  end
 end
+
+
+
+
+
